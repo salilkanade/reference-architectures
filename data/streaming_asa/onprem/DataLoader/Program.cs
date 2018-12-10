aaffa7ea7df040b4793be60208aa7fabd59476e5
Sid Ramadoss <38441940+sidramadoss@users.noreply.github.com>
2018-12-10 14:17:58 -0800

    Update Program.cs

--- data/streaming_asa/onprem/DataLoader/Program.cs
+++ data/streaming_asa/onprem/DataLoader/Program.cs
@@ -226,9 +226,11 @@ namespace Taxi
             var numberOfMillisecondsToLead = (int.TryParse(Environment.GetEnvironmentVariable("MINUTES_TO_LEAD"), out int outputMinutesToLead) ? outputMinutesToLead : 0) * 60000;
             var pushRideDataFirst = bool.TryParse(Environment.GetEnvironmentVariable("PUSH_RIDE_DATA_FIRST"), out Boolean outputPushRideDataFirst) ? outputPushRideDataFirst : false;*/
 
-            var rideConnectionString = "Endpoint=sb://forrester-demo-ehns.servicebus.windows.net/;SharedAccessKeyName=taxi-ride-asa-access-policy;SharedAccessKey=zb3Dt0tQXVwEo6Bz0VjVVrRBC2aKccH0UrUv1+NEHEM=;EntityPath=taxi-ride";
-            var fareConnectionString = "Endpoint=sb://forrester-demo-ehns.servicebus.windows.net/;SharedAccessKeyName=taxi-fare-asa-access-policy;SharedAccessKey=n7PF7TdtrDUvJMlXFVFg5mkQ9ez5735nENJGEnDbT2c=;EntityPath=taxi-fare";
+            //var rideConnectionString = "Endpoint=sb://forrester-demo-ehns.servicebus.windows.net/;SharedAccessKeyName=taxi-ride-asa-access-policy;SharedAccessKey=zb3Dt0tQXVwEo6Bz0VjVVrRBC2aKccH0UrUv1+NEHEM=;EntityPath=taxi-ride";
+            //var fareConnectionString = "Endpoint=sb://forrester-demo-ehns.servicebus.windows.net/;SharedAccessKeyName=taxi-fare-asa-access-policy;SharedAccessKey=n7PF7TdtrDUvJMlXFVFg5mkQ9ez5735nENJGEnDbT2c=;EntityPath=taxi-fare";
             //var rideDataFilePath = @"C:\Users\sidram\Downloads\reference-architectures-master\reference-architectures-master\data\streaming_asa\DataFile\FOIL2013";
+            var rideConnectionString = Environment.GetEnvironmentVariable("RIDE_EVENT_HUB");
+            var fareConnectionString = Environment.GetEnvironmentVariable("FARE_EVENT_HUB");
             var rideDataFilePath = Environment.GetEnvironmentVariable("RIDE_DATA_FILE_PATH");
             var numberOfMillisecondsToRun = (int.TryParse(Environment.GetEnvironmentVariable("SECONDS_TO_RUN"), out int outputSecondToRun) ? outputSecondToRun : 0) * 1000;
             var numberOfMillisecondsToLead = (int.TryParse(Environment.GetEnvironmentVariable("MINUTES_TO_LEAD"), out int outputMinutesToLead) ? outputMinutesToLead : 0) * 60000;
